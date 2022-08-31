6bc53b2c31f18cb2d86bab731f0278e7de726bae
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-08-31 18:07:56 -0400

    created Lmabdasian class with an object passed

--- index.js
+++ index.js
@@ -116,7 +116,14 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor({name, age, location}) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
