b6e52fc52868ae52f7794bdd97d3fc8f1d8515d4
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-08-31 18:55:18 -0400

    added class ProjectManager and 2 methods standUp & debugsCode

--- index.js
+++ index.js
@@ -204,8 +204,18 @@ class Student extends Lambdasian{
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
+class ProjectManager extends Instructor {
+   constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}) {
+    super({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+   }
+   standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+   }
+   debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+   }
 }
 
 /*
